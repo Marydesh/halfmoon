ce9f16b73b8c269ea7663bca2d1622820722f8ab
Maryna <serdeshnayamarina@gmail.com>
2023-10-16 23:53:15 -0700

    Co-authored-by: dugalcedo <dugalcedo@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # halfmoon
+![The screenshot of the webpage](./assets/scrensht%201.png)
+![The screenshot2 of the webpage](./assets/scrnsht%202.png)
+![The screenshot2 of the webpage](./assets/scrnsht%203.png)
+
+* JavaScript Simple Quiz
+
+Created a simple application - quiz.
+
+- Used the 'hide' classes to append to pages taht I want to hide-display in a sertaing moment/case.
+
+- Created a button for each answer option and used objects to crete answers/options.
+
+- Added event listeners to check if the answer is correct on a click of a button.
+
+- Creted a timer that deducts the time by 5sec if the answer was incorrect.
+
+- Creted a high score table and the initial submittion form taht stores the data in the local storage.
+
+
+# I got a lot of assistance on this projest form the TAs in class and the bootcamp tutors:
+
+- Yi Jin
+- Mila Hose
+
+# As well as ffound some answers while doing codecadamy Javascript full course.
+
+It is getting more challenging for me ... but I wont give up !
+
+Thnaks for checking this out!
--- script.js
+++ script.js
@@ -1,65 +1,185 @@
 var startPage = document.querySelector("#start-page")
 var startButton = document.querySelector("#start-button")
 var questionPage = document.querySelector("#question-page")
+var hiScoreTable = document.querySelector("#hi-score table")
+var timerElement = document.querySelector("#timer")
+var score = 0
 var questionCount = 0
-var questions = [
+var time = 60
+var timer
+var questions = [ /*source for questions - https://www.interviewbit.com/javascript-mcq/ and https://www.w3schools.com/quiztest/quiztest.asp?qtest=JS.
+Created an array of questions with options for answers and a correct answer for each question */
     {
-        question: "some question",
-        options: ["one", "two", "three", "four"],
-        correctAnswer: "two",
+        question: "Which operator is used to assign a value to a variable?",
+        options: ["*", "?", "-", "="],
+        correctAnswer: "=",
     },
     {
-        question: "some question2 ",
-        options: ["one", "two", "three", "four"],
-        correctAnswer: "one",
+        question: "How do we write a comment in javascript?",
+        options: ["*", "//", "#", "$$"],
+        correctAnswer: "//",
     },
     {
-        question: "some question3 ",
-        options: ["one", "two", "three", "four"],
-        correctAnswer: "two",
+        question: "How to stop an interval timer in Javascript? ",
+        options: ["stopInterval", "clearInterval", "intervalClear", "inervalOver"],
+        correctAnswer: "clearInterval",
     },
     {
-        question: "some question4 ",
-        options: ["one", "two", "three", "four"],
-        correctAnswer: "four",
+        question: "Which function is used to serialize an object into a JSON string in Javascript?",
+        options: ["stringify()", "convert()", "parseInt()", "string()"],
+        correctAnswer: "stringify()",
     }
 ]
+/* Added an event listener to the start button, when the button is clicked, first start page becomes invisible and the question page with the question appears, then the function to show a quetsion is calles/executed */
 
-startButton.addEventListener("click", function(){
+startButton.addEventListener("click", function () {
     startPage.classList.add("hide")
     questionPage.classList.remove("hide")
     showQuestion()
-}) 
-
+    startTimer()
+})
+// function to show quetsions and the answer options as buttons
 function showQuestion() {
     questionPage.innerHTML = "";
-    var question = document.createElement ("h2")
-    question.textContent = questions[questionCount].question
-    questionPage.append (question)
-    for (let i = 0; i < questions[questionCount].options.length; i++) {
+    var questionElement = document.createElement("h2")
+    var currentQuestion = questions[questionCount]
+    questionElement.textContent = currentQuestion.question
+    questionPage.append(questionElement)
+    for (let i = 0; i < currentQuestion.options.length; i++) {
         var button = document.createElement("button")
-        button.textContent = questions[questionCount].options[i]
-        button.value = questions[questionCount].options[i]
-        button.addEventListener("click",checkAnswer)
-        questionPage.append (button)
-        
+        button.textContent = currentQuestion.options[i]
+        button.value = currentQuestion.options[i]
+        button.addEventListener("click", checkAnswer)
+        button.style.margin = "2px"
+        button.style.width = "150px"
+        button.style.height = "40px"
+        button.style.borderRadius = "15px"
+        button.style.backgroundColor = "rgb(148, 91, 148)"
+        questionPage.append(button)
     }
 }
+//function to check the answer  - correct or wrong and display the right
 function checkAnswer(event) {
-    var feedback = document.createElement ("p")
+    var feedback = document.createElement("p")
     if (questions[questionCount].correctAnswer === event.target.value) {
-feedback.textContent = "Correct!"
-    }else {
+        // To Handle correct answer
+        feedback.textContent = "Correct!"
+        score++;
+    } else {
+        // To Handle wrong answer
         feedback.textContent = "Wrong!"
+        time -= 5
     }
-    questionPage.append (feedback)
-    setTimeout(function(){
+    questionPage.append(feedback)
+    setTimeout(function () {
         feedback.remove()
         questionCount++
         if (questionCount < questions.length) {
-            showQuestion ()
+            showQuestion()
         } else {
-         // show in screen   
+            showFinalScore() //calling a function to display a final score when no questions left
         }
-    },2000)
+    }, 1000)
 }
+//function to show the final score > calling create a form function and stopping the timer > show high scores
+function showFinalScore() {
+    questionPage.innerHTML = "";
+    var quizCompletedMessage = document.createElement("h2");
+    quizCompletedMessage.textContent = "All done!";
+    var scoreDisplay = document.createElement("p");
+    scoreDisplay.textContent = "Your final score is " + score;
+    questionPage.append(quizCompletedMessage, scoreDisplay);
+    createScoreForm()
+    stopTimer()
+    var highScoresSection = document.querySelector("#hi-score");
+    highScoresSection.classList.remove("hide");
+    showHighScores();
+}
+// function to creating a form to input the initials
+function createScoreForm() {
+    var form = document.createElement("form")
+    form.insertAdjacentHTML("beforeend", "<h2>Enter your intials</h2>") // beforeend - inserting the element inside the element, after its last child.
+    var input = document.createElement("input")
+    input.setAttribute('name', 'initials')
+    var button = document.createElement("button")
+    button.textContent = "Submit"
+    form.addEventListener('submit', submitInitials)
+    form.append(input, button)
+    questionPage.append(form)
+}
+// a function to submit this form 
+function submitInitials(event) {
+    event.preventDefault()
+    let initials = event.target.initials.value
+    let scores = getHighScores()
+    scores.push([initials, score])
+    localStorage.setItem('high-scores', JSON.stringify(scores))
+    window.location.reload()
+}
+// getting a high score form the local storage, if no scores , nothing will be returned
+function getHighScores() {
+    let scores = localStorage.getItem('high-scores')
+    // if scores doesn't exist
+    if (!scores) {
+        return []
+    } else {
+        return JSON.parse(scores)
+    }
+}
+// Function to display a score. Creates a table and inserts the players score and their name, initials into ${x[0]} and ${x[1]}//
+function showHighScores() {
+    let scores = getHighScores()
+    scores.forEach(x => {
+        hiScoreTable.insertAdjacentHTML('beforeend', ` 
+            <tr>
+                <td>${x[0]}</td>
+                <td>${x[1]}</td>
+            </tr>
+        `)
+    })
+}
+//timer function
+function startTimer() {
+    time = 60
+    timer = setInterval(() => {
+        time--
+        checkTime()
+        renderTimer()
+    }, 1000)
+}
+//  if there is no time left - show final score, stop the timer
+function checkTime() {
+    if (time <= 0) {
+        showFinalScore()
+        stopTimer()
+    }
+}
+
+function stopTimer() {
+    clearInterval(timer)
+}
+
+function renderTimer() {
+    timerElement.textContent = ":" + (time < 10 ? "0" : "") + time
+}
+
+showHighScores()
+
+//AS A coding boot camp student
+// I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+// SO THAT I can gauge my progress compared to my peers
+
+
+// Acceptance Criteria
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
